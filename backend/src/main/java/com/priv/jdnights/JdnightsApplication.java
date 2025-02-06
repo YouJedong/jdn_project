d20c9c620f18ec20f36db2ea5883c9ded6414f35
youjedong <>
2025-02-06 23:45:43 +0900

    add: first commit

---
+++ backend/src/main/java/com/priv/jdnights/JdnightsApplication.java
@@ -0,0 +1,13 @@
+package com.priv.jdnights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdnightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdnightsApplication.class, args);
+	}
+
+}
