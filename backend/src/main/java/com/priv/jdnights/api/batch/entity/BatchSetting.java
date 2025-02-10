2af84fa5ae811d1f40a058a5f3728c15298cdc1d
YouJedong <mraiou1@naver.com>
2025-02-11 00:19:36 +0900

    update: 배치 만들기2(미완)

--- backend/src/main/java/com/priv/jdnights/api/batch/entity/BatchSetting.java
+++ backend/src/main/java/com/priv/jdnights/api/batch/entity/BatchSetting.java
@@ -16,14 +16,19 @@ import java.time.LocalDateTime;
 public class BatchSetting extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String batchName; // 배치 이름
+    @Column(nullable = false, columnDefinition = "VARCHAR(4) COMMENT '배치 이름 타입'")
+    private String batchNameType; // 배치 이름 타입
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(50) COMMENT '실행 스케줄 (크론)'")
     private String cronExpression; // 실행 스케줄 (크론)
+
     private LocalDateTime lastRunDate; // 마지막 실행 시간
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false, columnDefinition = "VARCHAR(10) COMMENT '배치 활성 상태'")
     private BatchStatus status;
 
 }
--- backend/src/main/java/com/priv/jdnights/api/batch/service/ContentsBatchService.java
+++ backend/src/main/java/com/priv/jdnights/api/batch/service/ContentsBatchService.java
@@ -1,7 +1,9 @@
 package com.priv.jdnights.api.batch.service;
 
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 @Transactional
 public class ContentsBatchService {
 
@@ -9,6 +11,7 @@ public class ContentsBatchService {
     public void getNextClassContentsInfo() {
 
         //https://nextclass.co.kr/_next/data/Ji3TML8Lk1FtZ6kasPUNf/creator/90.json?index=90&coursePage=3
+
 
 
     }
