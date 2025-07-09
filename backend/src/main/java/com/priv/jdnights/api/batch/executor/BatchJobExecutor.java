cebfb04b73d3fdb5c9aebd1a1254ebd276e0a925
YouJedong <mraiou1@naver.com>
2025-07-09 22:42:12 +0900

    update: 스케줄러 설정

--- backend/src/main/java/com/priv/jdnights/api/batch/executor/BatchJobExecutor.java
+++ backend/src/main/java/com/priv/jdnights/api/batch/executor/BatchJobExecutor.java
@@ -4,6 +4,7 @@ import com.priv.jdnights.api.batch.service.FullScoreBatchService;
 import com.priv.jdnights.api.batch.service.NextClassBatchService;
 import com.priv.jdnights.api.batch.service.YouTubeBatchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,16 +19,18 @@ public class BatchJobExecutor {
     @Autowired
     private FullScoreBatchService fullScoreBatchService;
 
+    @Scheduled(cron = "0 0 1 * * *")
     public void getYouTubeContentsInfo() {
         youTubeBatchService.getYouTubeContentsInfo();
     }
 
     // 넥스트 클래스
-//    @Scheduled(cron = "* 27 23 * * ?")
+    @Scheduled(cron = "0 10 1 * * *")
     public void getNextClassContentsInfo() {
         nextClassBatchService.getNextClassContentsInfo();
     }
 
+    @Scheduled(cron = "0 20 1 * * *")
     // 풀스코어
     public void getFullScoreContentsInfo() {
         fullScoreBatchService.getFullScoreContentsInfo();
